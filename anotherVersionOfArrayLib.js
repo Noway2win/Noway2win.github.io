2cd0004dc83a2bb5055b0a98a1afd6e37613d443
Satsukevich Dzmitry <dimamoving@gmail.com>
2021-06-23 00:04:13 +0300

    added secind variant without iife

---
+++ anotherVersionOfArrayLib.js
@@ -0,0 +1,236 @@
+function arrayLib() {
+
+	var returnedObg = this || {};
+
+	returnedObg.chain = function (array) {
+
+		if (!arrayCheck(array)) {
+			return;
+		}
+
+
+		returnedObg.array = array;
+		return returnedObg;
+	};
+
+	returnedObg.take = function () {
+		var array, n;
+
+		var valuesObject = argumentsCheck(arguments);
+		array = valuesObject.array;
+		n = valuesObject.serviceVar;
+
+		if (!arrayCheck(array) || !lengthCheck(array, n)) {
+			console.log('error');
+			return;
+		}
+
+		var newArr = [];
+		for (var i = 0; i < n; i++) {
+			newArr.push(array[i]);
+		}
+		returnedObg.array = newArr;
+
+		return (arguments.length == 1) ? returnedObg : newArr;
+	};
+
+	returnedObg.skip = function () {
+		var array, n;
+
+		var valuesObject = argumentsCheck(arguments);
+		array = valuesObject.array;
+		n = valuesObject.serviceVar;
+
+		if (!arrayCheck(array) || !lengthCheck(array, n)) {
+			console.log('error');
+			return;
+		}
+
+		var newArr = [];
+
+		for (var i = n; i < array.length; i++) {
+			newArr.push(array[i]);
+		}
+
+		returnedObg.array = newArr;
+
+		return (arguments.length == 1) ? returnedObg : newArr;
+	};
+
+	returnedObg.map = function () {
+		var array, callback;
+
+		var valuesObject = argumentsCheck(arguments);
+		array = valuesObject.array;
+		callback = valuesObject.serviceVar;
+
+		if (!arrayCheck(array) || !functionCheck(callback)) {
+			console.log('error');
+			return;
+		}
+
+		var newArr = [];
+
+		for (var i = 0; i < array.length; i++) {
+			var newValue = callback(array[i]);
+			newArr.push(newValue);
+		}
+
+		returnedObg.array = newArr;
+
+		return (arguments.length == 1) ? returnedObg : newArr;
+	};
+
+	returnedObg.reduce = function () {
+		var valuesObject = argumentsCheck(arguments);
+		var array = valuesObject.array,
+			callback = valuesObject.serviceVar,
+			initialValue = valuesObject.additionalValue || 0;
+
+		if (!arrayCheck(array) || !functionCheck(callback)) {
+			console.log('error');
+			return;
+		}
+
+		for (var i = 0; i < array.length; i++) {
+			initialValue = callback(initialValue, array[i]);
+		}
+
+		return initialValue;
+	};
+
+	returnedObg.filter = function () {
+		var array, callback;
+
+		var valuesObject = argumentsCheck(arguments);
+		array = valuesObject.array;
+		callback = valuesObject.serviceVar;
+
+		if (!arrayCheck(array) || !functionCheck(callback)) {
+			console.log('error');
+			return;
+		}
+		var newArr = [];
+
+		for (var i = 0; i < array.length; i++) {
+			if (callback(array[i])) {
+				newArr.push(array[i]);
+			}
+		}
+
+		returnedObg.array = newArr;
+
+		return (arguments.length == 1) ? returnedObg : newArr;
+	};
+
+
+	returnedObg.forEach = function () {
+		var array, callback;
+
+		var valuesObject = argumentsCheck(arguments);
+		array = valuesObject.array;
+		callback = valuesObject.serviceVar;
+
+		if (!arrayCheck(array) || !functionCheck(callback)) {
+			console.log('error');
+			return;
+		}
+
+		for (var i = 0; i < array.length; i++) {
+			callback(array[i]);
+		}
+
+		return (arguments.length == 1) ? returnedObg : null;
+	};
+
+	returnedObg.value =
+		function () {
+			return returnedObg.array || [];
+		};
+
+	function arrayCheck(array) {
+		if (array.constructor != Array || array.length == 0) {
+			return false;
+		}
+		return true;
+	}
+
+	function lengthCheck(array, n) {
+		if (array.length != undefined && n > array.length) {
+			return false;
+		}
+		return true;
+	}
+
+	function functionCheck(func) {
+		if (func.constructor != Function) {
+			return false;
+		}
+		return true;
+	}
+
+	function argumentsCheck(args) {
+
+		var array, serviceVar, additionalValue;
+
+		switch (args.length) {
+			case 1: serviceVar = args[0];
+				array = returnedObg.array;
+				break;
+			case 2:
+				array = args[0];
+				serviceVar = args[1];
+				break;
+			case 3: array = args[0];
+				serviceVar = args[1];
+				additionalValue = args[2];
+				break;
+			default: array = [];
+				serviceVar = 0;
+				additionalValue = 0;
+				break;
+		}
+
+
+		return {
+			array: array,
+			serviceVar: serviceVar,
+			additionalValue: additionalValue
+		};
+
+	}
+
+	return returnedObg;
+}
+
+var lib = new arrayLib();
+
+console.log(lib.take([1, 2, 3, 4, 5, 6], 5));
+
+console.log(lib.chain([1, 2, 3, 4, 5, 6]).take(5).skip(1).map(a => a * 2).filter(a => a > 3).forEach(a => { console.log(a); }));
+
+console.log(lib.chain([1, 2, 3, 4, 5, 6]).take(5).skip(1).map(a => a * 2).filter(a => a > 3).value());
+
+console.log(lib.skip([1, 2, 3, 4, 5, 6], 5));
+
+console.log(lib.map([1, 2, 3, 4, 5, 6], a => a * 2));
+
+console.log(lib.chain([1, 2, 3, 4, 5, 6]).take(5).skip(1).reduce((a, b) => a + b));
+
+console.log(lib.reduce([1, 2, 3, 4, 5, 6], (a, b) => a + b));
+
+console.log(lib.reduce([1, 2, 3, 4, 5, 6], (a, b) => a + b, 5));
+
+console.log(lib.reduce([1, 2, 3, 4, 5, 6], 55));
+
+console.log(lib.filter([1, 2, 3, 4, 5, 6], a => a > 3));
+
+console.log(lib.forEach([1, 2, 3, 4, 5, 6], a => { console.log(a); }));
+
+var anotherLib = new arrayLib();
+
+console.log(anotherLib.value());
+
+var returnFromThirdLib = new arrayLib().chain([1, 2, 3, 4, 5, 6]).take(5).skip(1).value();
+
+console.log(returnFromThirdLib);
