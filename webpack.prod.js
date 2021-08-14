4b5c0c1dec27ff7dc9b30924aecebfc3c19b8335
Satsukevich Dzmitry <dimamoving@gmail.com>
2021-08-14 11:51:30 +0300

    v3

--- webpack.prod.js
+++ webpack.prod.js
@@ -25,17 +25,6 @@ module.exports = merge(config, {
 	module: {
 		rules: [
 			{
-				test: /\.m?js$/,
-				exclude: /(node_modules|bower_components)/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/preset-env'],
-						plugins: ['@babel/plugin-proposal-object-rest-spread']
-					}
-				}
-			},
-			{
 				test: /\.css$/,
 				use: [MiniCssExtractPlugin.loader, 'css-loader'],
 			}
